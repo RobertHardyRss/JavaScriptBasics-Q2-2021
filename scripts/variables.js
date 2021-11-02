c499e88c33e88c5a188f569d806bd018cb4381be
--global <--global>
2021-11-02 17:21:42 -0600

    Add latest changes to variable.js

--- scripts/variables.js
+++ scripts/variables.js
@@ -6,4 +6,11 @@
 let age = 16; // the data type of age is a number
 let fullName = "Robert Hardy"; // the data type of fullName is string
 
-//fullName.
+let isActive = true; // the data type of isActive is boolean (true or false)
+
+age = "16"; // age is now a string
+age = false; // age is a boolean
+
+const MAX_AGE = 200;
+
+MAX_AGE = 100;
