d645582f35dbdcf72674de903ca58ff63c4822a6
--global <--global>
2021-11-08 16:37:12 -0700

    Added canvas to template

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,12 @@
 /** @type {HTMLCanvasElement} */
-const canvas = document.getElementById('game-canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+
+let x = 400;
+let y = 300;
+
+//ctx.fillRect(x, y, 50, 50);
+
+ctx.arc(x, y, 50, 0, Math.PI * 2, false);
