4ce1003c01a127ae047ff8d55d10714cee55b8e8
--global <--global>
2021-11-10 10:11:14 -0700

    Add event listener and rainbow drawing

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,15 @@ canvas.height = 600;
 let x = 400;
 let y = 300;
 
-//ctx.fillRect(x, y, 50, 50);
+// ctx.fillRect(x, y, 50, 50);
 
-ctx.arc(x, y, 50, 0, Math.PI * 2, false);
+let color = 0;
+
+canvas.addEventListener("mousemove", (e) => {
+	// console.log(e);
+	ctx.fillStyle = `hsla(${color}, 100%, 50%, 1)`;
+	ctx.beginPath();
+	ctx.arc(e.offsetX, e.offsetY, 50, 0, Math.PI * 2, false);
+	ctx.fill();
+	color = color + 10;
+});
