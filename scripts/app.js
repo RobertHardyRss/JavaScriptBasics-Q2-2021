a6b0ebd28d2518db90de1dd37d4c0cfdfa5b05b3
--global <--global>
2021-11-10 18:04:06 -0700

    add conditionals to draw when the mouse is pressed and change colors

--- scripts/app.js
+++ scripts/app.js
@@ -6,16 +6,40 @@ canvas.height = 600;
 
 let x = 400;
 let y = 300;
-
-// ctx.fillRect(x, y, 50, 50);
-
 let color = 0;
+let lightness = 50;
+let isDrawing = false;
+
+window.addEventListener("keydown", (e) => {
+	//console.log(e.key);
+
+	switch (e.key) {
+		case "e":
+			lightness = 100;
+			break;
+
+		case "g":
+			color = 120;
+		default:
+			lightness = 50;
+	}
+});
+
+canvas.addEventListener("mousedown", () => {
+	isDrawing = true;
+});
+
+canvas.addEventListener("mouseup", () => {
+	isDrawing = false;
+});
 
 canvas.addEventListener("mousemove", (e) => {
 	// console.log(e);
-	ctx.fillStyle = `hsla(${color}, 100%, 50%, 1)`;
+	if (!isDrawing) return;
+
+	ctx.fillStyle = `hsla(${color}, 100%, ${lightness}%, 1)`;
 	ctx.beginPath();
 	ctx.arc(e.offsetX, e.offsetY, 50, 0, Math.PI * 2, false);
 	ctx.fill();
-	color = color + 10;
+	//color = color + 10;
 });
